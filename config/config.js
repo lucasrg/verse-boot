48c2168095bbaeb30b5531953f320c8cba872cc4
Lucas Goraieb <lucas.goraieb@gmail.com>
2016-08-08 11:48:49 -0700

    Styles directory structure

--- config/config.js
+++ config/config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  includes: {
+    js: 'bundle',
+    css: 'main'
+  },
   version: {
     js: 1,
-    css: 1,
+    css: 1
   }
 }
--- config/less.js
+++ config/less.js
@@ -4,16 +4,19 @@ var fs    = require('fs');
 var writefile = require('writefile')
 var config = require('./config');
 
-function compile(lessName, changedFileName) {
-  fs.readFile('./app/styles/' + lessName + '.less', function (err, data) {
+var publicPath = __dirname+'/../public/static/css/';
+var outputFile = publicPath + config.includes.css +'.' + config.version.css + '.css';
+
+function compile(lessMainFilename, changedFileName) {
+  fs.readFile(lessMainFilename, function (err, data) {
     var dataString = data.toString();
     if (!dataString) {
       console.info('[Less] main.less is empty.');
       return;
     }
     less.render(dataString, {
-      paths: [__dirname + '/../app/styles/'],
-      filename: lessName + '.less',
+      paths: [__dirname + '/../app/'],
+      filename: lessMainFilename,
       sourceMap: {
         sourceMapFileInline: true
       }
@@ -22,14 +25,14 @@ function compile(lessName, changedFileName) {
         console.error('[Less] ' + err.type + ' Error: ' + err.message + ' in ' + err.filename + ':' + err.index + ':' + err.line);
         return;
       }
-      writefile(__dirname+'/../public/static/css/' + lessName +'.' + config.version.css + '.css', output.css, function (err) {
+      writefile(outputFile, output.css, function (err) {
         if (err) {
           console.error('[Less] failed to write', err);
         } else {
           if (changedFileName) {
-            console.info('[Less] Changed ' + changedFileName + ', ' + lessName + '.css rendered.');
+            console.info('[Less] Changed ' + changedFileName);
           } else {
-            console.info('[Less] ' + lessName + '.css rendered.');
+            console.info('[Less] Changed Styles.less.');
           }
         }
       });
@@ -37,20 +40,14 @@ function compile(lessName, changedFileName) {
   });
 }
 
-var renderLess = function (changedFileName) {
-  compile('main',changedFileName);
-};
-
-// Watch for css changes
-var watchCallback = function (event, filename) {
-  var ext = filename.split('/').pop().split('.').pop();
-  if (ext !== 'less') return;
-  renderLess(filename);
-};
-
 module.exports = function () {
-  fs.watch('./app/styles', watchCallback);
-  fs.watch('./app/styles/pages', watchCallback);
-  fs.watch('./app/styles/components', watchCallback);
-  renderLess();
+  fs.watch('./app/Styles.less', function (event, filename) {
+    compile('./app/Styles.less', filename);
+  });
+
+  fs.watch('./app/styles/', { recursive: true }, function (event, filename) {
+    var ext = filename.split('/').pop().split('.').pop();
+    if (ext !== 'less') return;
+    compile('./app/Styles.less', filename);
+  });
 }
